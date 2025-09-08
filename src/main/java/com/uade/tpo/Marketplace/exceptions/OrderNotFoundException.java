31a4adb78f6638ad9068ac11a2b8747fd3491ac1
sofibesada <sofbesada@gmail.com>
2025-09-07 21:48:59 -0300

    cambiosExcepciones

--- src/main/java/com/uade/tpo/Marketplace/exceptions/OrderNotFoundException.java
+++ src/main/java/com/uade/tpo/Marketplace/exceptions/OrderNotFoundException.java
@@ -1,5 +1,8 @@
 package com.uade.tpo.Marketplace.exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class OrderNotFoundException {
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "La orden no se encuentra")
+public class OrderNotFoundException extends Exception{
     
 }
--- src/main/java/com/uade/tpo/Marketplace/exceptions/UserDuplicateException.java
+++ src/main/java/com/uade/tpo/Marketplace/exceptions/UserDuplicateException.java
@@ -3,5 +3,5 @@ package com.uade.tpo.Marketplace.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El producto que se intenta agregar está duplicado")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El user que se intenta agregar está duplicado")
 public class UserDuplicateException extends Exception { }
